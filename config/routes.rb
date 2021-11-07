f87f2746764d7a27198c81bc3c27d7d956f6268e
gabrielgiroe1 <gabriel.giroe@gmail.com>
2021-11-07 10:56:31 +0200

    first_commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  root 'articles#index'
+  resources :articles do
+    resources :comments
+  end
+
+  # get '/articles', to: 'articles#index'
+  # get '/articles/:id', to: 'articles#show'
+end
